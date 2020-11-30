3225658cfc8174c66fc58b1e18f58fc3622d7eb8
benji <benaja.bendo02@gmail.com>
2020-11-30 14:15:21 +0100

    creation des tables

--- app/Http/Controllers/API/UserController.php
+++ app/Http/Controllers/API/UserController.php
@@ -2,10 +2,66 @@
 
 namespace App\Http\Controllers\API;
 
+use App\Actions\Fortify\CreateNewUser;
+use App\Actions\Fortify\UpdateUserProfileInformation;
 use App\Http\Controllers\Controller;
+use App\Http\Resources\UserRessource;
+use App\Models\User;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 
 class UserController extends Controller
 {
-    //
+    //save User
+    public function register(Request $request)
+    {
+        $user = new CreateNewUser();
+        $user_connect = $user->create($request->all());
+
+        return response()->json([
+            'success' => 'utilisateur cree avec success',
+            'token' => $user_connect->api_token
+        ], 200);
+
+    }
+
+    //login user
+    public function login()
+    {
+        $isconnect = auth()->attempt([
+            'email' => request('email'),
+            'password' => request('password'),
+        ]);
+
+        if ($isconnect) {
+            return [
+                'success' => 'utilisateur conecté avec success',
+                'token' => Auth::user()->api_token
+            ];
+        } else {
+            return [
+                'error' => 'aucun utilisateur trouvé'
+            ];
+        }
+    }
+
+    //show all user
+    public function index()
+    {
+        return UserRessource::collection(User::all());
+    }
+
+    //update user
+    public function update(Request $request, $token)
+    {
+
+        $user = new UpdateUserProfileInformation();
+        $user->update();
+//        dd($token);
+        $user = User::findOrFail($token);
+        $user->name = $request->title;
+        if ($user->save()) {
+            return new UserRessource($user);
+        }
+    }
 }
--- routes/api.php
+++ routes/api.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\API\UserController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -14,6 +15,13 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
-    return $request->user();
-});
+//Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
+//    return $request->user();
+//});
+Route::get('user',[UserController::class,'index']);
+
+Route::post('user/login',[UserController::class,'login']);
+
+Route::post('user/register',[UserController::class,'register']);
+
+Route::put('user/update/{token}',[UserController::class,'update']);
